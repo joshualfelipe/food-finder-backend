86737fe338dc8cbf5cfb1e996ec8515605c48d4b
joshualfelipe <90828277+joshualfelipe @ users.noreply.github.com>
2026-04-29 20:28:49 +0800

    feat: add config for geoapify api and url

--- api/config.py
+++ api/config.py
@@ -5,6 +5,10 @@ class Settings(BaseSettings):
     FOURSQUARE_API_KEY: str
     FOURSQUARE_BASE_URL: str
     FOURSQUARE_X_PLACES_API_VERSION: str
+
+    GEOAPIFY_API_KEY: str
+    GEOAPIFY_BASE_URL: str
+
     OPENAI_BASE_URL: str
     OPENAI_API_KEY: str
 
