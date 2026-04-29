fdcaa4dff7ad213f779c0051724cbcf1e9ec2d94
joshualfelipe <90828277+joshualfelipe @ users.noreply.github.com>
2026-04-29 21:12:04 +0800

    feat: add initial geoapify api restaurant functionalities

--- api/controller/recommendations_controller.py
+++ api/controller/recommendations_controller.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 from dto.recommendations_dto import PlacesRequest, PlacesFilter
-from handler import restaurant_recommendations_handler
+from handler import (
+    restaurant_recommendations_handler,
+    geoapify_handler,
+    ai_chat_handler,
+)
 
 router = APIRouter(tags=["Places"])
 
@@ -29,3 +33,17 @@ async def recommendations(
     return await restaurant_recommendations_handler.recommendations(
         location, user_message, filters
     )
+
+
+@router.get("/geoapify")
+async def geoapify(latitude: float, longitude: float, user_message: str | None = None):
+    features = ai_chat_handler.resolve_search_parameters(user_message)
+    location = PlacesRequest(latitude=latitude, longitude=longitude)
+    return await geoapify_handler.fetch_geoapify_places(
+        location, user_message, features
+    )
+
+
+@router.get("/test/chat")
+async def test_chat_recommendations(user_message: str):
+    return ai_chat_handler.resolve_search_parameters(user_message)
